41bc74ff957ec54b207184129e7c8e2689eadb8a
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-01 00:53:56 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,1 +1,46 @@
 #include <stdio.h>
+/*
+ *Write a program that prints all possible combinations of two two-digit numbers.
+ *The numbers should range from 0 to 99
+ *The two numbers should be separated by a space
+ *All numbers should be printed with two digits. 1 should be printed as 01
+ *The combination of numbers must be separated by comma, followed by a space
+ *The combinations of numbers should be printed in ascending order
+ *00 01 and 01 00 are considered as the same combination of the numbers 0 and 1
+ *You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
+ *You can only use putchar eight times maximum in your code
+ *You are not allowed to use any variable of type char
+ *All your code should be in the main function
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int first; /*first digit*/
+	int second; /* second digit*/
+
+	for (first=0; first<99; first++)
+	{
+		for (second=0; second<100; second++)
+		{
+			if (first !=second && first < second)
+			{
+				putchar((first/10) + '0');
+				putchar((first%10) + '0');
+				putchar(' ');
+				putchar((second/10) + '0');
+				putchar((second%10)+ '0');
+				
+				if (first==98)
+					continue;
+
+			putchar(',');
+			putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return(0);
+}
+
