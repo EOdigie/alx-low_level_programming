c323dbf5c45f5f76c33efc6acbb914282b64d435
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-01 15:20:26 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
-/*Write a program that prints all possible different combinations of two digits.
- *Numbers must be separated by ,, followed by a space
- *The two digits must be different
- *01 and 10 are considered the same combination of the two digits 0 and 1
- *Print only the smallest combination of two digits
- *Numbers should be printed in ascending order, with two digits
- *You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- *You can only use putchar five times maximum in your code
- *You are not allowed to use any variable of type char
- *All your code should be in the main function
+/*
+ * main - Prints all possible different combinations of two digits
+ *
+ * Return: Always 0
  */
 int main(void)
 {
-	int a; //first digit
-	int b; //second digit
-	for (a=0; a<9;a++) // for loop for the first digit
-	{ 
-		for (b=0; b<10; b++) // for loop for the second digit
-		{
+	int a;
+	int b;
 
-			if (a!=b && a<b) // if statement - if first digit is not the same as second digit and first digit is less than second digit 
+	for (a = 0; a < 9;a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			if (a != b && a < b)
 			{
-				putchar(a%10+'0');putchar(b%10+'0');
+				putchar((a % 10)+'0');
+				putchar((b % 10)+'0');
 				{
-					if (a==8)
+					if (a == 8)
 						continue;
 				}
-				putchar(',');
-				putchar(' ');	
+				putchar (',');
+				putchar (' ');
 			}
-		
 		}
-	
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,15 +1,9 @@
 #include <stdio.h>
 /*
- * Write a program that prints all possible different combinations of three digits.
+ *main - Prints all possible different combinations of three digits.
  *Numbers must be separated by ,, followed by a space
- *The three digits must be different
- *012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2
- *Print only the smallest combination of three digits
- *Numbers should be printed in ascending order, with three digits
- *You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- *You can only use putchar six times maximum in your code
- *You are not allowed to use any variable of type char
- *All your code should be in the main function
+ *
+ * Return: Always 0
  */
 int main(void)
 {
@@ -17,30 +11,28 @@ int main(void)
 	int second;
 	int third;
 
-	for (first=0; first<8; first++)
+	for (first = 0; first < 8; first++)
 	{
-		for (second=0; second<9; second++)
+		for (second = 0; second < 9; second++)
 		{
-			for (third=0; third<10; third++)
+			for (third = 0; third < 10; third++)
 			{
-				if (first!=second && second!=third && first!=third)
+				if (first != second && second != third && first != third)
 				{
-					if(first<second && second<third)
+					if (first < second && second < third)
 					{
-						putchar(first%10 + '0');
-						putchar(second%10 +'0');
-						putchar(third%10 + '0');
-					
-						if (first==7)
+						putchar ((first % 10) + '0');
+						putchar ((second % 10) + '0');
+						putchar ((third % 10) + '0');
+						if (first == 7)
 							continue;
-
-						putchar(',');
-						putchar(' ');
+						putchar (',');
+						putchar (' ');
 					}
 				}
 			}
 		}
 	}
-	putchar('\n');
-	return(0);
+	putchar ('\n');
+	return (0);
 }
