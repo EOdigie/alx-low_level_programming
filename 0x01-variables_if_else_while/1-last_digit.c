23bdf2d50fc7c6a091ae5a33d9b4e9bf51c96a6f
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-01 08:35:11 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@
 int main(void)
 {
 	int n;
-
+	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
@@ -36,6 +36,5 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, n % 10);
 	}
-	
 	return (0);
 }
