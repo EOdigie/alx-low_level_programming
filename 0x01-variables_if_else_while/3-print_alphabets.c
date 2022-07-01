27dee2504e0cab2c11889ac2662525e727fce199
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-01 14:36:03 -0700

    0x01. C - Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,11 +10,11 @@ int main(void)
 	char CH;
 	
 	{
-		for (ch='a'; ch <='z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 			putchar(ch);
-		for (CH='A'; CH <='Z'; CH++)
+		for (CH = 'A'; CH <= 'Z'; CH++)
 			putchar(CH);
-		putchar('\n');
+		putchar ('\n');
 	}
 	return (0);
 }
