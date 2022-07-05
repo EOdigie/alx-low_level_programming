38dca15063370cd114d07a71c76079c3f8a2d16b
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-05 16:18:31 -0700

    0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,16 @@
 #include "main.h"
+/**
+ * main - print alphabets in lower case
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+        int c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        {
+                _putchar(c);
+        }
+_putchar('\n');
+}
