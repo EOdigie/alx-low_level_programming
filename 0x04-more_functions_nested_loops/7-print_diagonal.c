ba632e5ce12ba28c0e655ad34f1cd15ebbad1950
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-08 00:14:11 -0700

    0x04. C - More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_diagonal - draws diagonal line
+ * @n: int
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, a;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	for (i = 1; i <= n; i++)
+	{
+		a = 0;
+		while (a < i)
+		{
+			_putchar(' ');
+			a++;
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+}
