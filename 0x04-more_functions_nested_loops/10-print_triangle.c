c9bdb50555ec7bc07cc9a06bbcc4a4b218919a0b
Edugie Odigie <edugie.idemudia@gmail.com>
2022-07-08 02:26:51 -0700

    0x04. C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,11 +8,13 @@
 void print_triangle(int size)
 {
 	int a, b;
-	
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 	} else
+
+	{
 		for (a = 1; a <= size; a++)
 		{
 			for (b = a; b < size; b++)
@@ -27,4 +29,5 @@ void print_triangle(int size)
 
 			_putchar('\n');
 		}
+	}
 }
